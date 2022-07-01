9b3e80ea433a8aa0f12bcbcff5c95868fc21f28b
JSantilli <5582387+JSantilli@users.noreply.github.com>
2022-07-01 00:24:13 -0600

    Added wait key

--- eventHandler.js
+++ eventHandler.js
@@ -2,31 +2,36 @@
 
 'use strict';
 
-import { BumpAction } from "./action.js";
+import { BumpAction, WaitAction } from "./action.js";
 
 export class EventHandler {
 
 	game;
-	keyMap;
+	directionKeys;
+	waitKeys;
 	
 	constructor(game) {
 
 		this.game = game;
 
-		this.keyMap = {};
-		this.keyMap[ROT.KEYS.VK_UP] = 0;
-		this.keyMap[ROT.KEYS.VK_RIGHT] = 2;
-		this.keyMap[ROT.KEYS.VK_DOWN] = 4;
-		this.keyMap[ROT.KEYS.VK_LEFT] = 6;
+		this.directionKeys = {};
+		this.directionKeys[ROT.KEYS.VK_UP] = 0;
+		this.directionKeys[ROT.KEYS.VK_RIGHT] = 2;
+		this.directionKeys[ROT.KEYS.VK_DOWN] = 4;
+		this.directionKeys[ROT.KEYS.VK_LEFT] = 6;
 
-		this.keyMap[ROT.KEYS.VK_I] = 0;
-		this.keyMap[ROT.KEYS.VK_O] = 1;
-		this.keyMap[ROT.KEYS.VK_L] = 2;
-		this.keyMap[ROT.KEYS.VK_PERIOD] = 3;
-		this.keyMap[ROT.KEYS.VK_COMMA] = 4;
-		this.keyMap[ROT.KEYS.VK_M] = 5;
-		this.keyMap[ROT.KEYS.VK_J] = 6;
-		this.keyMap[ROT.KEYS.VK_U] = 7;
+		this.directionKeys[ROT.KEYS.VK_I] = 0;
+		this.directionKeys[ROT.KEYS.VK_O] = 1;
+		this.directionKeys[ROT.KEYS.VK_L] = 2;
+		this.directionKeys[ROT.KEYS.VK_PERIOD] = 3;
+		this.directionKeys[ROT.KEYS.VK_COMMA] = 4;
+		this.directionKeys[ROT.KEYS.VK_M] = 5;
+		this.directionKeys[ROT.KEYS.VK_J] = 6;
+		this.directionKeys[ROT.KEYS.VK_U] = 7;
+
+		this.waitKeys = {};
+		this.waitKeys[ROT.KEYS.VK_K] = true;
+		// TODO: This structure seems a little funky. This 'true' is totally unnecessary for the logic, but has to be here syntactically.
 
 		window.addEventListener("keydown", this);
 	}
@@ -40,11 +45,12 @@ export class EventHandler {
 	event_keydown(e) {
 		const keyCode = e.keyCode;
 
-		if (!(keyCode in this.keyMap)) { return; } // invalid input
-
-		const [dx, dy] = ROT.DIRS[8][this.keyMap[keyCode]];
-
-		new BumpAction(this.game.map.player, dx, dy).perform();
+		if (keyCode in this.directionKeys) {
+			const [dx, dy] = ROT.DIRS[8][this.directionKeys[keyCode]];
+			new BumpAction(this.game.map.player, dx, dy).perform();
+		} else if (keyCode in this.waitKeys) {
+			new WaitAction(this.game.map.player).perform();
+		}
 		
 		this.game.engine.unlock();
 	}
